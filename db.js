752dff64f83c3a8918a49c1e2530fbd0005454e7
marisasuchithra-ux <marisasuchithra@gmail.com>
2026-03-31 22:26:34 +1100

    Initial commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_RVf0b9TPEFeu@ep-dark-truth-a7t29bqw-pooler.ap-southeast-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
